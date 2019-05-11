6193a4a214830e3ff180fe3ace3172d826af40d9
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-05-10 23:04:38 -0700

    Pre-game cleanup

--- app/src/main/java/com/rubyhuntersky/indexrebellion/interactions/refreshholdings/RefreshHoldingsKt.kt
+++ app/src/main/java/com/rubyhuntersky/indexrebellion/interactions/refreshholdings/RefreshHoldingsKt.kt
@@ -1,47 +1,49 @@
 package com.rubyhuntersky.indexrebellion.interactions.refreshholdings
 
-import android.util.Log
 import com.rubyhuntersky.interaction.core.*
 import com.rubyhuntersky.robinhood.api.RbhApi
 import com.rubyhuntersky.robinhood.api.results.RbhHoldingsResult
 
-sealed class RefreshHoldingsVision {
-    object Idle : RefreshHoldingsVision()
-    object AwaitingResult : RefreshHoldingsVision()
-    data class Error(val message: String) : RefreshHoldingsVision()
-    data class Holdings(val holdings: RbhHoldingsResult) : RefreshHoldingsVision()
+
+private fun start(): Vision = Vision.Idle
+
+sealed class Vision {
+    object Idle : Vision()
+    object AwaitingResult : Vision()
+    data class Error(val message: String) : Vision()
+    data class Holdings(val holdings: RbhHoldingsResult) : Vision()
 }
 
-sealed class RefreshHoldingsAction {
-    data class Start(val token: String, val api: RbhApi, val id: Long) : RefreshHoldingsAction()
-    data class Error(val throwable: Throwable) : RefreshHoldingsAction()
-    data class Result(val result: RbhHoldingsResult) : RefreshHoldingsAction()
-}
+private fun isTail(_: Any?) = false
 
-const val REFRESH_HOLDINGS = "RefreshHoldings"
-
-private typealias UpdateResult = WellResult<RefreshHoldingsVision, RefreshHoldingsAction>
-
-private fun update(vision: RefreshHoldingsVision, action: RefreshHoldingsAction): UpdateResult {
-    return if (vision is RefreshHoldingsVision.Idle && action is RefreshHoldingsAction.Start) {
-        val holdingsWish = Wish<RefreshHoldingsAction>(
-            action = action.api.holdings(action.token)
-                .map(RefreshHoldingsAction::Result)
-                .cast(RefreshHoldingsAction::class.java)
-                .onErrorReturn(RefreshHoldingsAction::Error),
-            name = "holdings${action.id}"
-        )
-        UpdateResult(RefreshHoldingsVision.AwaitingResult, holdingsWish)
-    } else if (vision is RefreshHoldingsVision.AwaitingResult && action is RefreshHoldingsAction.Error) {
-        val throwable = action.throwable
-        Log.e(REFRESH_HOLDINGS, "Holdings fetch failed: ${throwable.localizedMessage}", throwable)
-        UpdateResult(RefreshHoldingsVision.Error(throwable.localizedMessage))
-    } else if (vision is RefreshHoldingsVision.AwaitingResult && action is RefreshHoldingsAction.Result) {
-        UpdateResult(RefreshHoldingsVision.Holdings(action.result))
-    } else {
-        throw IllegalStateException("$vision denies $action")
+private fun update(vision: Vision, action: Action): WellResult<Vision, Action> {
+    return when {
+        vision is Vision.Idle && action is Action.Start -> {
+            val holdingsWish = Wish<Action>(
+                action = action.api.holdings(action.token)
+                    .map { Action.ReceiveResult(it) as Action }
+                    .onErrorReturn(Action::ReceiveError),
+                name = "holdings${action.id}"
+            )
+            WellResult(Vision.AwaitingResult, holdingsWish)
+        }
+        vision is Vision.AwaitingResult && action is Action.ReceiveError -> {
+            WellResult(Vision.Error(action.throwable.localizedMessage))
+        }
+        vision is Vision.AwaitingResult && action is Action.ReceiveResult -> {
+            WellResult(Vision.Holdings(action.result))
+        }
+        else -> throw IllegalStateException("$vision denies $action")
     }
 }
 
-class RefreshHoldingsInteraction(well: Well) : Interaction<RefreshHoldingsVision, RefreshHoldingsAction>
-by WellInteraction(well, { RefreshHoldingsVision.Idle }, ::update, { false }, REFRESH_HOLDINGS)
+sealed class Action {
+    data class Start(val token: String, val api: RbhApi, val id: Long) : Action()
+    data class ReceiveError(val throwable: Throwable) : Action()
+    data class ReceiveResult(val result: RbhHoldingsResult) : Action()
+}
+
+class RefreshHoldingsInteraction(well: Well) : Interaction<Vision, Action>
+by WellInteraction(well, ::start, ::update, ::isTail, REFRESH_HOLDINGS)
+
+const val REFRESH_HOLDINGS = "RefreshHoldings"
