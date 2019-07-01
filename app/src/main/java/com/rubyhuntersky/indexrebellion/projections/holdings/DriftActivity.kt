886281f708d026874be835cea983a228357087c2
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-07-01 07:58:22 -0700

    Separate adjustments when displayed in DriftActivity

--- app/src/main/java/com/rubyhuntersky/indexrebellion/projections/holdings/DriftActivity.kt
+++ app/src/main/java/com/rubyhuntersky/indexrebellion/projections/holdings/DriftActivity.kt
@@ -3,6 +3,7 @@ package com.rubyhuntersky.indexrebellion.projections.holdings
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.rubyhuntersky.indexrebellion.data.techtonic.Drift
+import com.rubyhuntersky.indexrebellion.data.techtonic.plating.PlateAdjustment
 import com.rubyhuntersky.indexrebellion.data.techtonic.vault.Custodian
 import com.rubyhuntersky.indexrebellion.interactions.holdings.Action
 import com.rubyhuntersky.indexrebellion.interactions.holdings.HoldingsStory
@@ -17,6 +18,7 @@ import com.rubyhuntersky.vx.coop.additions.mapSight
 import com.rubyhuntersky.vx.tower.additions.augment.extendCeiling
 import com.rubyhuntersky.vx.tower.additions.inCoop
 import com.rubyhuntersky.vx.tower.additions.mapSight
+import com.rubyhuntersky.vx.tower.additions.replicate.replicate
 
 class DriftActivity : AppCompatActivity() {
 
@@ -39,25 +41,33 @@ class DriftActivity : AppCompatActivity() {
 
     companion object {
 
-        private val holdingsContentTower = MultiHoldingTower.extendCeiling(BalanceTower)
-            .mapSight { drift: Drift ->
-                PageSight(
-                    balance = "0,00",
-                    holdings = drift.generalHoldings.map {
-                        HoldingSight(
-                            name = drift.market.findSample(it.instrumentId)!!.instrumentName,
-                            custodians = it.custodians.map(Custodian::toString),
-                            count = it.size,
-                            symbol = it.instrumentId.symbol,
-                            value = it.cashValue!!.value
-                        )
-                    }
-                )
+        private val holdingsContentTower = MultiHoldingTower.extendCeiling(BalanceTower).mapSight { drift: Drift ->
+            PageSight(
+                balance = "0,00",
+                holdings = drift.generalHoldings.map {
+                    HoldingSight(
+                        name = drift.market.findSample(it.instrumentId)!!.instrumentName,
+                        custodians = it.custodians.map(Custodian::toString),
+                        count = it.size,
+                        symbol = it.instrumentId.symbol,
+                        value = it.cashValue!!.value
+                    )
+                }
+            )
+        }
+
+        private val adjustmentTower = Standard.BodyTower().mapSight { adjustment: PlateAdjustment ->
+            adjustment.toString()
+        }
+
+        private val adjustmentsContentTower =
+            adjustmentTower.replicate().neverEvent<Nothing>().mapSight { drift: Drift ->
+                drift.plateAdjustments.toList()
             }
 
         private val pageTower = Standard.SectionTower(
             Pair("Holdings", holdingsContentTower),
-            Pair("Adjustments", Standard.BodyTower().mapSight { drift: Drift -> drift.plateAdjustments.toString() })
+            Pair("Adjustments", adjustmentsContentTower)
         )
 
         private val pageCoop: Coop<Vision.Viewing, Nothing> = pageTower.inCoop().mapSight(Vision.Viewing::drift)
